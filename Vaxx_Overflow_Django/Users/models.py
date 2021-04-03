182042f70dbc9706e603693e4b52cd8c9918c5b6
Daniel Manfield <>
2021-04-03 01:53:13 -0400

    Models Updated

--- Vaxx_Overflow_Django/Users/models.py
+++ Vaxx_Overflow_Django/Users/models.py
@@ -5,14 +5,60 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 #Just a basic model to build off of
 
-class Users(models.Model):
-    first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=25)
-    email = models.EmailField()
-    phone_number = PhoneNumberField()
-    zipcode = models.IntegerField()
-    date_of_birth = models.DateField()
-    job = #CHOICES
-    age = models.IntegerField()
+#Also we could just use the built in user model..... just thought of that
+
+
+
+PHASE_CHOICES = (
+    ("1B": "1B"),
+    ("1C": "1C")
+)
+
+VACCINE_CHOICES = (
+    ("Pfizer-BioNTech": "Pfizer-BioNTech"),
+    ("Moderna": "Moderna"),
+    ("Johnson & Johnson")
+)
+
+GENDER_CHOICES = (
+    ("Male": "Male"),
+    ("Female": "Female"),
+    ("Other": "Other")
+)
+
+OCCUPATION_CHOICES = (
+    ("Transportation": "Transportation"),
+    ("Logistics": "Logistics"),
+    ("Educational": "Educational"),
+    ("Postal Service": "Postal Service"),
+    ("Food Service": "Food Service"),
+    ("Construction": "Construction"),
+    ("Finance": "Finance"),
+    ("Information Technology": "Information Technology"),
+    ("Communications": "Communications"),
+    ("Energy": "Energy"),
+    ("Firefighter": "Firefighter"),
+    ("Law Enforcement": "Law Enforcement"),
+    ("Media": "Media"),
+    ("Public Safety": "Public Safety"),
+    ("Public Health": "Public Health")
+)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=25, null=False)
+    last_name = models.CharField(max_length=25, null=False)
+    email = models.EmailField(null=False)
+    gender = models.CharField(choices=GENDER_CHOICES)
+    phone_number = PhoneNumberField(null=False)
+    zipcode = models.IntegerField(null=False)
+    date_of_birth = models.DateField(null=False)
+    age = models.IntegerField(null=False)
+    
+
+
+    job = models.CharField(choices=OCCUPATION_CHOICES)
+    phase_category = models.CharField(choices=PHASE_CHOICES)
     health_info = #Might need to make this into multiple parts.
+    vaccine_brands = models.CharField(choices=VACCINE_CHOICES)
 
