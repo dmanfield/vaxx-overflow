5b95cf35c694a24cab1ecad663a7491a4f476aa0
Daniel Manfield <74741982+dmanfield@users.noreply.github.com>
2021-04-03 20:39:17 -0400

    issues with file name

--- Vaxx_Overflow_Django/Users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import People
-# Register your models here.
-
-
-class PeopleAdmin(admin.ModelAdmin):
-    fields = ["first_name", "last_name", "email", "gender", "phone_number", "zipcode", "date_of_birth", "job", "group_living", "vaccine_brands", "health_condition"]
-
-admin.site.register(People, PeopleAdmin)
--- Vaxx_Overflow_Django/Users/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import People
-
-class PeopleForm(forms.ModelForm):
-    class Meta:
-        model = People
-        fields = ("first_name", "last_name", "email", "gender", "phone_number", "zipcode", "date_of_birth", "job", "group_living", "vaccine_brands", "health_condition")
-
