4e160545f6ae8f24a70bc0fa54519b73ed515125
Brenden Schumacher <btschumacher19@gmail.com>
2021-04-04 15:53:48 -0400

    fixed screening page one

--- Vaxx_Overflow_Django/vaxx-overflow-app/src/pages/user/URegScreening.js
+++ Vaxx_Overflow_Django/vaxx-overflow-app/src/pages/user/URegScreening.js
@@ -1,4 +1,4 @@
-import PrimaryButton from '../../components/Button/PrimaryButton';
+import SecondaryButton from '../../components/Button/SecondaryButton';
 import CheckBox from '../../components/CheckBox/InlineCheckbox';
 // import InputField from '../../components/InputField/InputField';
 
@@ -9,12 +9,11 @@ function UserScreeningPage() {
     <header className="frame-1">
       <h1 id="vaxx">Vaxx</h1>
       <h1 id="overflow">OVERFLOW</h1>
+      <br></br>
+      <h3 id="heading-paragraph">Next, fill out a screening form to check your elegibilty with local and CDC guidelines.</h3>
     </header>
-    <div>
-      <h3>
-        Next, fill out a screening form to check your elegibilty with local and CDC guidelines.
-      </h3>
-    </div>
+<br></br>
+<br></br>
     <div className="long-con">
       <div className="long-box">
         <CheckBox label={"1 - Have you recieved a Covid-19 authorization?"}></CheckBox>
@@ -29,7 +28,9 @@ function UserScreeningPage() {
        <CheckBox label={"4 - Do you have chronic health conditions such as: Cancer, kidney disease, autoimmune disease, chronic lung disease, obesity, sickle cell disease, diabetes, heart disease, or any chronic condition?"}></CheckBox>
        </div>
     </div>
-
+    <div id="providers-secondary-button">
+        <SecondaryButton text="next" id="secondary-button" />
+      </div>
   </div>
   )
 };
