1c9be154c3a10a94b70e215355c9f47b79021fed
Michael <michaelorland2@gmail.com>
2021-04-03 20:56:16 -0500

    people serializer

--- Vaxx_Overflow_Django/users/views.py
+++ Vaxx_Overflow_Django/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from knox.models import AuthToken
+from .serializers import PeopleSerializer
 
-# Create your views here.
+class PeopleView(APIView):
+
+    def post(self, request, format=None):
+        serializer = PeopleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
