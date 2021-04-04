b168c4615285895920d305d05797dc47db428bf2
Michael <michaelorland2@gmail.com>
2021-04-04 13:23:32 -0500

    user page 2

---
+++ Vaxx_Overflow_Django/vaxx-overflow-app/src/pages/user/URegPage2.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PrimaryButton from '../../components/Button/PrimaryButton';
+import InputField from '../../components/InputField/InputField';
+import DropForm from '../../components/DropForm/DropForm';
+
+
+const URegPageTwo =(props)=> {
+  
+  return (
+    <div className="centered-div">
+      <header className="frame-1">
+        <h1 id="vaxx">Vaxx</h1>
+        <h1 id="overflow">OVERFLOW</h1>
+        <h3 id="heading-paragraph">We first need to gather your personal and health demographic information.</h3>
+      </header>
+      <form className="initial-form">
+        <InputField placeholder="" label="Last Name"/>
+        <DropForm placeholder="" label="Gender" option={['Male', 'Female', 'Other']}/>
+        <InputField placeholder="" label="Phone"/>
+        <InputField placeholder="" label="Zipcode"/>
+        <InputField placeholder="" label="Date of Birth" />
+      </form>
+      <div id="main-primary-button">
+        <PrimaryButton  id="primary-button" text="SUBMIT" clickAction={ }/>
+      </div>
+    </div>
+  );
+};
+
+export default URegPageTwo;
