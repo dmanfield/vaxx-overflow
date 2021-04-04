628461ac2118dcda905fd153a94f52b6102ecfe0
Michael <michaelorland2@gmail.com>
2021-04-04 15:20:53 -0500

    user reg page 4

---
+++ Vaxx_Overflow_Django/vaxx-overflow-app/src/pages/user/URegPage4.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PrimaryButton from '../../components/Button/PrimaryButton';
+import CheckBox from '../../components/CheckBox/InlineCheckbox';
+
+
+
+const URegPageFour = (props) => {
+
+  return (
+    <div className="centered-div">
+      <header className="frame-1">
+        <h1 id="vaxx">Vaxx</h1>
+        <h1 id="overflow">OVERFLOW</h1>
+        <h3 id="heading-paragraph">Please answer the following to the best of your ability.</h3>
+      </header>
+      <div className="buttons-list">
+        <h4>Additional criteria for eligibility</h4>
+        <div style={{display: "grid"}}>
+          <CheckBox class="checkbox" label="School staff members or Childcare Workers"/>
+          <CheckBox class="checkbox" label="Healthcare Workers"/>
+          <CheckBox class="checkbox" label="Ages 16+"/>
+          <CheckBox class="checkbox" label="High Risk Medical Conditions"/>
+          <CheckBox class="checkbox" label="Residents or staff of congregate living facility"/>
+        </div>
+      </div>
+      <div id="main-primary-button" style={{padding: "100px"}}>
+        <PrimaryButton  id="primary-button" text="SUBMIT" clickAction={() => console.log(":)")}/>
+      </div>
+    </div>
+  );
+};
+
+export default URegPageFour; 
